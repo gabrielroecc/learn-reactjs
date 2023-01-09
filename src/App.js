ddbd9f65b027cf6a08bf9f333482548f6d8b02d9
Gabriel Roec <you@example.com>
2023-01-09 18:51:04 -0300

    JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
-import HelloWorld from "./components/HelloWorld";
 
 function App() {
+  const name = "Gab";
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const url = "https://via.placeholder.com/200";
+
   return (
     <div className="App">
-      <HelloWorld />
+      <h1>Understanding JSX</h1>
+      <p>Hello, {name}</p>
+      <p>Sum: {sum(10, 10)}</p>
+      <img src={url} />
     </div>
   );
 }
--- src/components/Frase.js
+++
@@ -1,8 +0,0 @@
-function Frase() {
-  return (
-    <div>
-      <p>Frase is phrase in portuguese</p>
-    </div>
-  );
-}
-export default Frase;
